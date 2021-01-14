a9656007b277a89f40424699f10db889feeaaa44
root <root@ip-10-1-2-165.us-east-2.compute.internal>
2021-01-14 13:25:58 +0000

    First commit

---
+++ mysql-example/script.sql/create_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE  blog(
+blog_id int(5),
+blog_category varchar(30),
+blog_name varchar(30),
+blog_rating int(5));
---
+++ mysql-example/script.sql/insert_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO blog VALUES(101,'ORACLE','Recover Scenario',4.5);
+INSERT INTO blog VALUES(102,'.NET','Docker Commands',5.5);
+INSERT INTO blog VALUES(103,'Terraform','Missnory of Terrafor',6.5);
+INSERT INTO blog VALUES(104,'JAVA','JAVA Scenario',7.5);
+
